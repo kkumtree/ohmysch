9a14465fca9e63e993c973df732f62ee89a801fd
hycho <hycho@student.42seoul.kr>
2020-05-04 08:50:05 +0900

    temp

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ohmysch
-trying to make scheduler for zsh-terminal. ramdomly print out such as To-do list, cellphones' plan list, stocks. 
+trying to make scheduler for zsh-terminal. ramdomly print out such as To-do list.
+
+## dependency
+- SQlite3 required
+	- sudo apt-get update 
+	- sudo apt-get install sqlite3 libsqlite3-dev
+- Github: Kaundur/cmdo required ( https://github.com/Kaundur/cmdo )
+	- (python 2 required)
+	- pip install cmdo-app 
+
+## Installation
+- cd ~
+- git clone git@github.com:kkumtree/ohmysch.git
+- echo "~/ohmysch/ohmysch" >> ~/.zshrc
+
+## etc
+- If additional function added to .c file, add flag like below when compiling.
+	- gcc -o ohmysch {sourcefiles} -lsqlite3
+
---
+++ ohmysch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sqlite3.h>
+ 
+int main(void)
+{ 
+    int rc;
+	rc = 0;
+    sqlite3 *db;
+	// char *path = "file://home/hycho/.local/bin/cmdo.db";
+	// when using absoulute path, error occured >> not fixed yet
+ 
+    rc = sqlite3_open("../.local/bin/cmdo.db", &db);
+ 
+    if(!rc)
+	{
+		printf("  _     ___ _____ _____   ___ ____    _____ ___  ____ _______   __  _______        _____  \n"); 
+		printf(" | |   |_ _|  ___| ____| |_ _/ ___|  |  ___/ _ \\|  _ |_   _\\ \\ / / |_   _\\ \\      / / _ \\ \n");
+		printf(" | |    | || |_  |  _|    | |\\___ \\  | |_ | | | | |_) || |  \\ V _____| |  \\ \\ /\\ / | | | |\n");
+		printf(" | |___ | ||  _| | |___   | | ___) | |  _|| |_| |  _ < | |   | |_____| |   \\ V  V /| |_| |\n");
+		printf(" |_____|___|_|   |_____| |___|____/  |_|   \\___/|_| \\_\\|_|   |_|     |_|    \\_/\\_/  \\___/ \n");
+		printf("\n");
+	}
+	else
+		printf("NEED initializing by enter 'cmdo' command OR CHECK sqlite3_open PATH\n"); 
+ 
+    sqlite3_close(db);
+
+    return 0;
+}
+
