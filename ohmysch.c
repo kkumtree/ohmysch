558e8f616a7ffc15cd0a1409cfab21bb6516caf9
hycho <hycho@student.42seoul.kr>
2020-05-05 17:29:49 +0900

    Using libft

--- ohmysch.c
+++ ohmysch.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/types.h>
 #include <sqlite3.h>
- 
-int main(void)
-{ 
-    int rc;
-	rc = 0;
-    sqlite3 *db;
-	// char *path = "file://home/hycho/.local/bin/cmdo.db";
-	// when using absoulute path, error occured >> not fixed yet
- 
-    rc = sqlite3_open("/home/hycho/.local/bin/cmdo.db", &db);
- 
+#include "libft.h"
+
+void	print_status(int rc)
+{
     if(!rc)
 	{
-		printf("  _     ___ _____ _____   ___ ____    _____ ___  ____ _______   __  _______        _____  \n"); 
-		printf(" | |   |_ _|  ___| ____| |_ _/ ___|  |  ___/ _ \\|  _ |_   _\\ \\ / / |_   _\\ \\      / / _ \\ \n");
-		printf(" | |    | || |_  |  _|    | |\\___ \\  | |_ | | | | |_) || |  \\ V _____| |  \\ \\ /\\ / | | | |\n");
-		printf(" | |___ | ||  _| | |___   | | ___) | |  _|| |_| |  _ < | |   | |_____| |   \\ V  V /| |_| |\n");
-		printf(" |_____|___|_|   |_____| |___|____/  |_|   \\___/|_| \\_\\|_|   |_|     |_|    \\_/\\_/  \\___/ \n");
-		printf("\n");
+		printf("███╗     ██╗███████╗███████╗    ██╗███████╗    ██╗  ██╗██████╗ \n");
+		printf("███║     ██║██╔════╝██╔════╝    ██║██╔════╝    ██║  ██║╚════██╗\n");
+		printf("███║     ██║█████╗  █████╗      ██║███████╗    ███████║ █████╔╝\n");
+		printf("███║     ██║██╔══╝  ██╔══╝      ██║╚════██║    ╚════██║██╔═══╝ \n");
+		printf("████████╗██║██║     ███████╗    ██║███████║         ██║███████╗\n");
+		printf(" ╚══════╝╚═╝╚═╝     ╚══════╝    ╚═╝╚══════╝         ╚═╝╚══════╝\n");                                                         
 	}
 	else
-		printf("NEED initializing by enter 'cmdo' command OR CHECK sqlite3_open PATH\n"); 
+	{
+		ft_putstr_fd("NEED initializing by enter 'cmdo' command OR CHECK sqlite3_open PATH\n", 1); 
+	}
+}
+
+int		main(void)
+{ 
+	char *db_path;
+	int rc;
+	rc = 0;
+    sqlite3 *db;
+
+/*
+**	get userid and userinfo
+**	http://forum.falinux.com/zbxe/index.php?document_srl=408418&mid=C_LIB
+**/
+
+	uid_t			user_id;
+   	struct passwd	*user_pw;
+
+	user_id  = getuid();            
+	user_pw  = getpwuid(user_id);  
+
+/*
+**	get path for cmdo.db
+*/
+
+	db_path = ft_strjoin(user_pw->pw_dir, "/.local/bin/cmdo.db");
+    rc = sqlite3_open(db_path, &db);
+ 
+	print_status(rc);
  
     sqlite3_close(db);
-
-    return 0;
+	free(db_path);
+    return (0);
 }
 
